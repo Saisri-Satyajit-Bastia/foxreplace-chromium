65a27b7509da0973489eeedacc2a81282a58ca39
Saisri <saisri.s.bastia@outlook.com>
2025-06-21 16:05:17 +0530

    Remove Firefox build scripts - not needed for Chrome extension

--- config_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Build config for build.sh
-APP_NAME=foxreplace
-CLEAN_UP=1
-ROOT_FILES="manifest.json"
-ROOT_DIRS="_locales icons lib options popup scripts sidebar"
-BEFORE_BUILD=
-AFTER_BUILD=
-EXCLUDED_EXTENSIONS="xcf"
